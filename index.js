8c612069bf0bf20b79cac938911db8859df7af58
Justin <justin.clv@gmail.com>
2013-05-27 18:45:10 -0700

    Structural changes. In progress of adding calls and number menus.

--- index.js
+++ index.js
@@ -39,30 +39,111 @@ app.set('view options', {
     layout: false
 });
 
-
+//Incoming call
 app.post('/call', function(req, res) {
-    //Validate that this request really came from Twilio...
-//	console.log(req.body);
 	io.sockets.emit('newNumber',{'obj':req.body });
-
 	phoneNumbers.push(req.body.From);
 	
 	var resp = new twilio.TwimlResponse();
 	resp.play(host+'/final.mp3');
 	resp.say({voice:'woman'},'Thank you for calling Airlert, from '+req.body.CallerCity+'. Your number is now added to the subscriber list for air quality alerts.');
-	resp.gather({timeout:30,action:host+'/gathered',numDigits:1},function(){
+	resp.gather({timeout:10,action:host+'/gathered',numDigits:1},function(){
 		this.say("Press a number and see it appear Live.")
 	});
 	res.type('text/xml');
 	res.send(resp.toString());
 });
 
+//Twiml responses
+app.post('/call/new', function(req, res) {
+	var resp = new twilio.TwimlResponse();
+	
+	resp.gather({timeout:60,action:host+'/response/1',numDigits:1},function(){
+		this.say({voice:'woman'},'Your smoke connector has gone off in your kitchen. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
+		.pause({ length:3 })
+		.say({voice:'woman', language:'en-gb'},'Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
+		.pause({ length:3 })
+		.say({voice:'woman'},'Your smoke connector has gone off in your kitchen. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
+		.pause({ length:3 })
+	});
+	
+    resp.gather({timeout:60,action:host+'/gathered',numDigits:1},function(){
+		this.say("Press a number and see it appear Live.")
+	});
+	res.type('text/xml');
+	res.send(resp.toString());
+	
+});
+
+app.post('/call/update', function(req, res) {
+	
+	
+});
+
+
+app.post('/response/1', function(req, res) {
+    //Validate that this request really came from Twilio...
+//	console.log(req.body);
+	
+	var resp = new twilio.TwimlResponse();
+	
+	resp.gather({timeout:60,action:host+'/response/2',numDigits:1},function(){
+		switch(req.body.Digits)
+		{
+			case 1:
+				this.say({voice:'woman'},'Phew! That was a close one. The rest of your household will be notified that this was just a false alarm. To change your response, press 7. Otherwise, tell your housemates what happened with a short recorded message. Leave your message after the tone and press # to send.
+				');
+				break;
+			case 3:
+				this.say({voice:'woman'},'Keep calm and stay on the line. The smoke connector has gone off. We\'re contacting your housemates to see what happened and will update you whether it\'s an emergency or false alarm. To change your response, press 7.
+				');
+				break;
+			case 9:
+				this.say({voice:'woman'},'Stay calm. We\'re alerting your housemates. Please call 9-1-1 immediately. To change your response, press 7. Otherwise, please hang up and dial 9-1-1.
+				');
+				break;
+		}
+	};
+	
+});
+
+app.post('/response/2', function(req, res) {
+	
+}
+
+
+
+
+
+
+
+
+//Incoming SMS incomingsms
+app.post('/incomingsms', function(req, res) {
+	io.sockets.emit('newmessage',{'obj':req.body });
+	var message=req.body.Body;
+	if(message.toLowerCase().match('fire')){
+		io.sockets.emit('update',{'response': 'yes','number':req.body.From});
+	}
+	else if(message.toLowerCase().match('false')){
+		io.sockets.emit('update',{'response': 'no','number':req.body.From});
+	}
+	else {
+		io.sockets.emit('update',{'response': 'Comment: '+message,'number':req.body.From});
+	}
+	
+	res.send('');
+});
+
 app.post('/gathered', function(req, res) {
     //Validate that this request really came from Twilio...
 //	console.log(req.body);
+	
 	var resp = new twilio.TwimlResponse();
-	resp.say({voice:'woman',language:'de'}, 'Hast du etwas Zeit für mich\? Dann singe ich ein Lied für dich, Von 9'+req.body.Digits+' Luftballons Auf ihrem Weg zum Horizont. Denkst du vielleicht gerad an mich Dann singe ich ein Lied für dich Von 9'+req.body.Digits+' Luftballons Und dass so was von so was kommt. ');
-	resp.say({voice:'woman',language:'es'}, 'Debe ser el '+req.body.Digits+' que usas o el agua con la que te bañas, pero cada cosita que haces, a mí me parece una hazaña, me besaste esa noche cual si fuera el único dia de tu boca 	y cada vez que me acuerdo yo siento en mi pecho el peso de una roca.');
+	resp.say({voice:'woman',language:'en'}, 'Thanks for your response.');
+	
+//	resp.say({voice:'woman',language:'de'}, 'Hast du etwas Zeit für mich\? Dann singe ich ein Lied für dich, Von 9'+req.body.Digits+' Luftballons Auf ihrem Weg zum Horizont. Denkst du vielleicht gerad an mich Dann singe ich ein Lied für dich Von 9'+req.body.Digits+' Luftballons Und dass so was von so was kommt. ');
+//	resp.say({voice:'woman',language:'es'}, 'Debe ser el '+req.body.Digits+' que usas o el agua con la que te bañas, pero cada cosita que haces, a mí me parece una hazaña, me besaste esa noche cual si fuera el único dia de tu boca 	y cada vez que me acuerdo yo siento en mi pecho el peso de una roca.');
 	io.sockets.emit('numberPress',{'theobject':req.body,'number': req.body.Digits});
 	console.log(req);
 	res.send(resp.toString());
@@ -98,7 +179,12 @@ app.get('/alert',function(request, responseHttp){
 });
 
 
-
+app.get('/status', function(req, res){
+	console.log(req.url);
+	res.render('status', {
+		server : host+':'+port
+	});
+});
 
 
 app.get('/', function(req, res){
