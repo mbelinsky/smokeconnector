8ebf628e394eb3efd3439a3dd11656ffda8c617c
Justin <justin.clv@gmail.com>
2013-05-31 00:44:03 -0700

    Revert "Revert "Restructured for demo. Need to fix sockets error""
    This reverts commit a1aeda3d55a4c892d3b7b1eee8b4dc68462982ca.

--- index.js
+++ index.js
@@ -34,14 +34,13 @@ app.configure('development', function(){
 	console.log('development');
 });
 
-
+var twilioNumber = '+14074776653';
 
 var justinNumber = "+13476819080";
 var markNumber = "+13474669327";
-var twilioNumber = '+14074776653';
-
 
 var phoneNumbers=[justinNumber];
+var phoneNumbers_es=[];
 
 //var twilio = require('twilio');
 //var client = new twilio.RestClient('twilio')('ACeac2f16de43f1d54afc199dc5f7ae200', '8d7f041fe6dd708664d01d472a2ed904');
@@ -87,19 +86,55 @@ app.post('/call', function(req, res) {
 	
 	var resp = new twilio.TwimlResponse();
 	resp.play(host+'/final.mp3');
-	resp.say({voice:'woman'},'Thank you for calling Airlert, from '+req.body.CallerCity+'. Your number is now added to the subscriber list for air quality alerts.');
+	resp.say({voice:'woman'},'Thank you for calling from '+req.body.CallerCity+'. To subscribe in Spanish, press 2');
 	resp.gather({timeout:10,action:host+'/gathered',numDigits:1},function(){
-		this.say("Press a number and see it appear Live.")
+		this.say({voice:'woman',language:'es'},"To subscribe in Spanish press 2.")
 	});
 	res.type('text/xml');
 	res.send(resp.toString());
 });
 
+
+
+app.post('/gathered', function(req, res) {
+    //Validate that this request really came from Twilio...
+//	console.log(req.body);
+	var choice=req.body.Digits;
+	
+	var resp = new twilio.TwimlResponse();
+	if(choice===1){
+		resp.say({voice:'woman',language:'en'}, 'Thanks for your response. You are now subscribed in English.');
+		phoneNumbers.push(req.body.From);
+	}
+	else if(choice===2){
+		resp.say({voice:'woman',language:'es'}, 'Thanks for your response. You are now subscribed in Spanish.');
+		phoneNumbers_es.push(req.body.From);
+	}
+	else {
+		resp.say({voice:'woman',language:'es'}, 'You have been subscribed in English by default');
+		phoneNumbers.push(req.body.From);
+	}
+	
+//	resp.say({voice:'woman',language:'de'}, 'Hast du etwas Zeit für mich\? Dann singe ich ein Lied für dich, Von 9'+req.body.Digits+' Luftballons Auf ihrem Weg zum Horizont. Denkst du vielleicht gerad an mich Dann singe ich ein Lied für dich Von 9'+req.body.Digits+' Luftballons Und dass so was von so was kommt. ');
+//	resp.say({voice:'woman',language:'es'}, 'Debe ser el '+req.body.Digits+' que usas o el agua con la que te bañas, pero cada cosita que haces, a mí me parece una hazaña, me besaste esa noche cual si fuera el único dia de tu boca 	y cada vez que me acuerdo yo siento en mi pecho el peso de una roca.');
+	io.sockets.emit('subscribed',{'obj':req.body,'language': req.body.Digits});
+	console.log(req);
+	res.send(resp.toString());
+});
+
+app.post('/gathered2', function(req, res) {
+	var choice=req.body.Digits;
+	var resp = new twilio.TwimlResponse();
+
+	res.send(resp.toString());
+});
+
+
 //Twiml response
 app.post('/call/new', function(req, res) {
 	var resp = new twilio.TwimlResponse();
 	
-	io.sockets.emit('logthis',{'obj':req.body,'msg':'New call initiated' });
+	io.sockets.emit('logthis',{'obj':req.body,'info':'New call initiated' });
 	
 	
 //	Called
@@ -127,63 +162,54 @@ app.post('/call/update', function(req, res) {
 //Used for updating status of each callee
 app.post('/call/ended', function(req,res){
 	//Call update status
-	
+	io.sockets.emit('logthis',{'obj':req.body,'info':'Call ended' });
 	
 });
 
 
 
-
-
 app.post('/response/1', function(req, res) {
 	
+	io.sockets.emit('logthis',{'obj':req.body,'info':'First response made' });
 	var number=req.body.Called;
-	
+
+	var resp = new twilio.TwimlResponse();
+
 	switch(req.body.Digits)
 	{
 		case 1:
-			// Assign false to DB entry with this number
+			io.sockets.emit('response',{'number':number,'response':'' });
+			// Assign false to DB entry with this number.
 			// Call updated response function
+			this.say({voice:'woman'},'Phew! That was a close one. The rest of your household will be notified that this was just a false alarm. To change your response, press 7. Otherwise, tell your housemates what happened with a short recorded message. Leave your message after the tone and press # to send.');
 			break;
 		case 3:
 			// Assign unsure to DB entry with this number
-			// Call updated response function
+			// Call updated response function			
+			this.say({voice:'woman'},'Keep calm and stay on the line. The smoke connector has gone off. We\'re contacting your housemates to see what happened and will update you whether it\'s an emergency or false alarm. To change your response, press 7.');
 			break;
 		case 9:
 			// Assign emergency to DB entry with this number
 			// Call updated response function
+			this.say({voice:'woman'},'Stay calm. We\'re alerting your housemates. Please call 9-1-1 immediately. To change your response, press 7. Otherwise, please hang up and dial 9-1-1.');
 			break;
 		default:
-			
+			this.say({voice:'woman'},'Text here - Obviously you were listening to our presentation and not what number you were suppose to press.');
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	var resp = new twilio.TwimlResponse();
-	
-	io.sockets.emit('logthis',{'obj':req.body });
-	
-	resp.gather({timeout:60,action:host+'/response/2',numDigits:1},function(){
+/*	resp.gather({timeout:60,action:host+'/response/2',numDigits:1},function(){
 		switch(req.body.Digits)
 		{
 			case 1:
-				this.say({voice:'woman'},'Phew! That was a close one. The rest of your household will be notified that this was just a false alarm. To change your response, press 7. Otherwise, tell your housemates what happened with a short recorded message. Leave your message after the tone and press # to send.');
 				break;
 			case 3:
-				this.say({voice:'woman'},'Keep calm and stay on the line. The smoke connector has gone off. We\'re contacting your housemates to see what happened and will update you whether it\'s an emergency or false alarm. To change your response, press 7.');
 				break;
 			case 9:
-				this.say({voice:'woman'},'Stay calm. We\'re alerting your housemates. Please call 9-1-1 immediately. To change your response, press 7. Otherwise, please hang up and dial 9-1-1.');
 				break;
 			default:
-				this.say({voice:'woman'},'Text here');
 		}
-	});
+	});*/
 	
 	
 	res.type('text/xml');
@@ -216,17 +242,7 @@ app.post('/incomingsms', function(req, res) {
 	res.send('');
 });
 
-app.post('/gathered', function(req, res) {
-    //Validate that this request really came from Twilio...
-//	console.log(req.body);
-	var resp = new twilio.TwimlResponse();
-	resp.say({voice:'woman',language:'en'}, 'Thanks for your response.');
-//	resp.say({voice:'woman',language:'de'}, 'Hast du etwas Zeit für mich\? Dann singe ich ein Lied für dich, Von 9'+req.body.Digits+' Luftballons Auf ihrem Weg zum Horizont. Denkst du vielleicht gerad an mich Dann singe ich ein Lied für dich Von 9'+req.body.Digits+' Luftballons Und dass so was von so was kommt. ');
-//	resp.say({voice:'woman',language:'es'}, 'Debe ser el '+req.body.Digits+' que usas o el agua con la que te bañas, pero cada cosita que haces, a mí me parece una hazaña, me besaste esa noche cual si fuera el único dia de tu boca 	y cada vez que me acuerdo yo siento en mi pecho el peso de una roca.');
-	io.sockets.emit('numberPress',{'theobject':req.body,'number': req.body.Digits});
-	console.log(req);
-	res.send(resp.toString());
-});
+
 
 
 
