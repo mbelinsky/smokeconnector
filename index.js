053f197e5c867b08c42045f95daffef81e18a04c
Justin <justin.clv@gmail.com>
2013-06-06 13:57:01 -0400

    Got flow working nice and smooth. Going to add .mp3s

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ var mongodb = require('mongodb');
 
 
 
-
-
 app.configure('development', function(){
 	port = 3000;
 	host='http://localhost';
@@ -38,6 +36,9 @@ var twilioNumber = '+14074776653';
 
 var justinNumber = "+13476819080";
 var markNumber = "+13474669327";
+
+
+var voice_url = host+'/voice/';
 
 var phoneNumbers=[];
 var phoneNumbers_es=[];
@@ -62,6 +63,31 @@ app.set('view options', {
 
 
 
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
 /*
 
 
@@ -83,14 +109,20 @@ send SMS's to people that are subscribed.
 
 
 //Incoming call
+
+
 app.post('/call', function(req, res) {
 	io.sockets.emit('newNumber',{'obj':req.body });
 	var resp = new twilio.TwimlResponse();
 	resp.play(host+'/final.mp3');
-	resp.say({voice:'woman'},'Thank you for calling from '+req.body.CallerCity+'.');
+	resp.play(voice_url+'welcome.mp3');
+	
+	resp.say({voice:'woman'},'Thank you for calling from '+req.body.CallerCity+'. ');
+	
 	resp.gather({timeout:10,action:host+'/gathered',numDigits:1},function(){
-		this.say({voice:'woman',language:'en'},"To subscribe in Spanish press 2.")
+		this.say({voice:'woman',language:'en'},"To subscribe in English press 1. Spanish press 2. French press 3.")
 	});
+	
 	res.type('text/xml');
 	res.send(resp.toString());
 });
@@ -100,38 +132,181 @@ app.post('/call', function(req, res) {
 app.post('/gathered', function(req, res) {
     //Validate that this request really came from Twilio...
 //	console.log(req.body);
-	var choice=parseInt(req.body.Digits);
+	var choice=req.body.Digits;
 	
-	var resp = new twilio.TwimlResponse();
-	if(choice===1){
-		resp.say({voice:'woman',language:'en'}, 'You are now subscribed in English.');
-		phoneContact.push({'number':req.body.From,'language':'en'});
+	if(choice==='*'){
+		client.calls(req.body.CallSid).post({
+		    url:host+'/call'
+		}, function(err, text) {});
 	}
-	else if(choice===2){
-		resp.say({voice:'woman',language:'en'}, 'You are now subscribed in Spanish.');
-		phoneContact.push({'number':req.body.From,'language':'es'});
+	
+	var lang='en';
+	if(choice==='1'){
+		lang='en';		
+	}
+	else if(choice==='2'){
+		lang='fr';		
 	}
 	else {
-		resp.say({voice:'woman',language:'en'}, 'You have been subscribed in English by default');
-		phoneContact.push({'number':req.body.From,'language':'en'});
+		lang='de';		
 	}
 	
-//	resp.say({voice:'woman',language:'de'}, 'Hast du etwas Zeit für mich\? Dann singe ich ein Lied für dich, Von 9'+req.body.Digits+' Luftballons Auf ihrem Weg zum Horizont. Denkst du vielleicht gerad an mich Dann singe ich ein Lied für dich Von 9'+req.body.Digits+' Luftballons Und dass so was von so was kommt. ');
-//	resp.say({voice:'woman',language:'es'}, 'Debe ser el '+req.body.Digits+' que usas o el agua con la que te bañas, pero cada cosita que haces, a mí me parece una hazaña, me besaste esa noche cual si fuera el único dia de tu boca 	y cada vez que me acuerdo yo siento en mi pecho el peso de una roca.');
-	io.sockets.emit('subscribed',{'number':req.body.From,'choice': choice});
-	console.log(req);
+	var resp = new twilio.TwimlResponse();
+//	resp.play(host+'/final_'+lang+'.mp3');
+	resp.gather( {timeout:30,action:host+'/gathered/2/'+lang,numDigits:1},function(){
+		this.say({voice:'woman',language:lang},"Thank you for calling. Pick a number and I will say it over and over again in my language.")
+//		this.play(host+'/final_'+lang+'.mp3');
+		});
+	
+//	phoneContact.push({'number':req.body.From,'language':lang});
+//	io.sockets.emit('subscribed',{'number':req.body.From,'choice': choice});
+	
 	res.send(resp.toString());
 });
 
-app.post('/gathered2', function(req, res) {
-	var choice=parseInt(req.body.Digits);
-	var resp = new twilio.TwimlResponse();
+app.post('/gathered/2/:language', function(req, res) {
+	var lang=req.params.language;
+	var choice=req.body.Digits;
 
+	if(choice==='*'){
+		client.calls(req.body.CallSid).post({
+		    url:host+'/call'
+		}, function(err, text) {});
+	} 
+	
+	var resp = new twilio.TwimlResponse();
+//	resp.play(host+'/title_'+lang+'.mp3');
+	
+	resp.gather({timeout:30,action:host+'/gathered/2/'+lang,numDigits:1},function(){
+		this.say({voice:'woman',language:lang}, choice +' and '+choice + ' and '+choice+ '. Ok now pick another number, or 5 for a high five');
+		this.play(host+'/final.mp3');
+	});
+		
+	if(choice==='5')
+		client.sms.messages.create({
+		    to:req.body.From,
+		    from:twilioNumber,
+		    body:'You pressed 5. High five! Oh and your language was '+lang
+		}, function(error, message) {});	
+	
 	res.send(resp.toString());
 });
 
 
 //Twiml response
+
+
+
+//Used for updating status of each callee
+app.post('/call/ended', function(req,res){
+	//Call update status
+	io.sockets.emit('logthis',{'obj':req.body,'info':'Call ended' });
+	
+});
+
+
+
+
+
+
+
+//Incoming SMS incomingsms
+app.post('/incomingsms', function(req, res) {
+	
+	var message=req.body.Body;
+	var zip = '10001';
+	var lang='en';
+	
+	if(message.match(/\d{5}/)){
+		zip = String(message.match(/\d{5}/));
+	}
+	else
+	{
+		zip = req.body.FromZip;	
+	}
+	var random = Math.floor(Math.random() * (160 - 50 + 1)) + 50;
+	
+	var smsResp='Thanks for signing up to Justin\'s Canary. The air quality at NYLS is Good: 42. The air quality in '+zip +' is OK: '+random+'. The pollen count is: Normal';
+	
+	
+	if(message.toLowerCase().indexOf('russian')!==-1){
+		lang = 'en-gb';
+		smsResp='Спасибо за регистрацию на Канарских. качество воздуха в ваше '+zip +'является OK: 54 пыльцы High.';
+	}
+	if(message.toLowerCase().indexOf('spanish')!==-1){
+		lang = 'es';
+		smsResp='Gracias por suscribirse al Canario. La calidad del aire en NYLS es Buena: 42. La calidad del aire en '+zip +' es OK: 54. La densidad polen es: Alta';
+	}
+	
+	
+	phoneContact.push({'number':req.body.From,'language':lang});
+
+
+	client.sms.messages.create({
+	    to:req.body.From,
+	    from:twilioNumber,
+	    body:smsResp
+	}, function(error, message) {
+	    if (!error) {
+			io.sockets.emit('logthis',{'obj':req.body.From,'info':'SMS sent' });
+	    }
+	    else {
+			io.sockets.emit('logthis',{'obj':req.body.From,'info':'Error sending' });
+	    }
+	});
+	io.sockets.emit('newContact',{'number':req.body.From,'zip':zip,'lang':lang  });
+	
+	io.sockets.emit('logthis',{'obj':zip,'info':'Zip extracted'});
+	
+	res.send('');
+});
+
+
+
+//Socket testing
+
+app.get('/sock',function(request, responseHttp){
+
+	console.log(request.body);
+	io.sockets.emit('testSock', { 'number':justinNumber, 'zip':'10001'});
+
+	responseHttp.send('Socket test message sent');// echo the result back});
+});
+
+
+app.get('/alerttest',function(request, responseHttp){
+	io.sockets.emit('hardAlert', { 'time':getDateTime()});
+	
+});
+
+app.get('/softalert',function(request, responseHttp){
+	console.log(request.body);
+	var timeNow=getDateTime();
+	io.sockets.emit('softAlert', { 'time':timeNow});
+	responseHttp.send('Soft alert. Subscribers: '+phoneContact.length+'. Time occurred: '+timeNow);// echo the result back});
+});
+
+app.get('/testdata/:phone', function(req, res){
+	
+	var data = phoneContact.filter(
+		function(contact){
+			return(contact.number===req.params.phone);
+		});
+		if (data.length>0){
+			res.send(data[0].language);
+		} else {
+			res.status(404).send('Number doesn\'t exist')
+		}
+});
+
+app.get('/test/:mytext/:number', function(req, res){
+	
+	io.sockets.emit('update',{'number':req.params.number,'status':req.params.mytext });
+
+	res.send('Sent socket message newEvent with number: '+ req.params.number+' status:'+req.params.mytext);
+});
+
+//Call out in response to real alert
 app.post('/call/new', function(req, res) {
 	var resp = new twilio.TwimlResponse();
 	
@@ -149,14 +324,12 @@ app.post('/call/new', function(req, res) {
 			lang=data[0].language;
 		}
 	
-	
-	
 	resp.gather({timeout:60,action:host+'/response/1',numDigits:1},function(){
-		this.say({voice:'woman', language:lang},'Your smoke connector has gone off in your kitchen. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
+		this.say({voice:'woman', language:lang},'Justin\'s Canary has reported a fire alarm at City Camp. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
 		.pause({ length:3 })
 		.say({voice:'woman', language:lang},'Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
 		.pause({ length:3 })
-		.say({voice:'woman', language:lang},'Your smoke connector has gone off in your kitchen. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
+		.say({voice:'woman', language:lang},'Justin\'s Canary has reported a fire alarm at City Camp. Press 1 if you know it\'s a false alarm. Press 3 if you are not sure. Press 9 if this is an emergency.')
 		.pause({ length:3 })
 	});
 	
@@ -164,21 +337,6 @@ app.post('/call/new', function(req, res) {
 	res.send(resp.toString());
 	
 });
-
-app.post('/call/update', function(req, res) {
-	
-	
-});
-
-
-//Used for updating status of each callee
-app.post('/call/ended', function(req,res){
-	//Call update status
-	io.sockets.emit('logthis',{'obj':req.body,'info':'Call ended' });
-	
-});
-
-
 
 app.post('/response/1', function(req, res) {
 	
@@ -190,20 +348,20 @@ app.post('/response/1', function(req, res) {
 	switch(choice)
 	{
 		case 1:
-			io.sockets.emit('update',{'number':number,'status':'False' });
+			io.sockets.emit('update',{'number':number,'status':'false' });
 			// Assign false to DB entry with this number.
 			// Call updated response function
 			resp.say({voice:'woman'},'Phew! That was a close one. The rest of your household will be notified that this was just a false alarm.');
 			break;
 		case 3:
-			io.sockets.emit('update',{'number':number,'status':'Not sure' });
+			io.sockets.emit('update',{'number':number,'status':'notsure' });
 			
 			// Assign unsure to DB entry with this number
 			// Call updated response function			
 			resp.say({voice:'woman'},'Keep calm. The smoke connector has gone off. We\'re contacting your housemates to see what happened.');
 			break;
 		case 9:
-			io.sockets.emit('update',{'number':number,'status':'Emergency' });
+			io.sockets.emit('update',{'number':number,'status':'emergency' });
 		
 			// Assign emergency to DB entry with this number
 			// Call updated response function
@@ -215,63 +373,20 @@ app.post('/response/1', function(req, res) {
 			resp.say({voice:'woman'},'Text here - Obviously you were listening to our presentation and not what number you were suppose to press.');
 	}
 	
-	
-/*	resp.gather({timeout:60,action:host+'/response/2',numDigits:1},function(){
-		switch(req.body.Digits)
-		{
-			case 1:
-				break;
-			case 3:
-				break;
-			case 9:
-				break;
-			default:
-		}
-	});*/
-	
-	
 	res.type('text/xml');
 	res.send(resp.toString());
 	
 	
 });
 
-app.post('/response/2', function(req, res) {
-	
-});
-
-
-
-
-//Incoming SMS incomingsms
-app.post('/incomingsms', function(req, res) {
-	io.sockets.emit('newSMS',{'obj':req.body });
-	var message=req.body.Body;
-	if(message.toLowerCase().match('fire')){
-		io.sockets.emit('update',{'response': 'yes','number':req.body.From});
-	}
-	else if(message.toLowerCase().match('false')){
-		io.sockets.emit('update',{'response': 'no','number':req.body.From});
-	}
-	else {
-		io.sockets.emit('update',{'response': 'Comment: '+message,'number':req.body.From});
-	}
-	
-	res.send('');
-});
-
-
-
-
-
+//Trigger real alert
 app.get('/alert',function(request, responseHttp){
 
 	console.log(request.body);
-	io.sockets.emit('alert', { 'timeReceived':23162});
+	io.sockets.emit('hardAlert', { 'time':getDateTime() });
 	
 	phoneContact.forEach(function(contact)
 	{
-		io.sockets.emit('logthis',{'obj':contact,'info':'Caller' });
 		
 		client.calls.create({
 		    url: host+'/call/new',
@@ -281,15 +396,13 @@ app.get('/alert',function(request, responseHttp){
 		}, function(err, call) {
 			if (!err) { // "err" is an error received during the request, if any
 		        console.log(call);
-				io.sockets.emit('update',{'number':contact.number,'status':'Calling' });
+				io.sockets.emit('update',{'number':contact.number,'status':'calling' });
 				}				
 			});
-		
 	});
 
-	responseHttp.send('Subscribers: '+phoneContact.length);// echo the result back});
+	responseHttp.send('Hard alert. Subscribers: '+phoneContact.length+'. Time occurred: '+getDateTime());// echo the result back});
 });
-
 
 app.get('/status', function(req, res){
 	console.log(req.url);
@@ -297,22 +410,6 @@ app.get('/status', function(req, res){
 		server : host+':'+port
 	});
 });
-
-
-
-
-app.get('/testdata/:phone', function(req, res){
-	var data = phoneContact.filter(
-		function(contact){
-			return(contact.number===req.params.phone);
-		});
-		if (data.length>0){
-			res.send(data[0].language);
-		} else {
-			res.status(404).send('Number doesn\'t exist')
-		}
-});
-
 
 app.get('/', function(req, res){
 	console.log(req.url);
