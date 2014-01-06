90b5b46e0113ef396f60c608fa4a110b359001d5
Justin <justin.clv@gmail.com>
2014-01-05 17:46:40 -0800

    Removed me

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var phoneContact=[];
 //phoneContact.push({'number':'14252885022','firstName':'Bruce T.','place':'WA',});
 phoneContact.push({'number':'15412922422','firstName':'Adonya','place':'OR'});
 phoneContact.push({'number':'12132680395','firstName':'Brady','place':'CA'});
-phoneContact.push({'number':'14159203651','firstName':'Justin','place':'CA'});
+//phoneContact.push({'number':'14159203651','firstName':'Justin','place':'CA'});
 phoneContact.push({'number':'13474669327','firstName':'Mark','place':'NY'});
 
 
