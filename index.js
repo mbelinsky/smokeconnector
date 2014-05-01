04db2e06f4ccc8c57cee6b4a3300f0f6589210f0
justLV <justin.clv@gmail.com>
2014-05-01 01:00:30 -0700

    Ask for date

--- index.js
+++ index.js
@@ -654,7 +654,13 @@ app.post('/v1/feeds/36ebcaadca18978f472ddfaa297e7fa2', function(req, responseHtt
 app.post('/post_test', function(req, responseHttp) {
 //	req.body.token
 	console.log("Got POST test: "+req.body.values);
-	responseHttp.send('I got that the CO2 was '+req.body.values);
+	responseHttp.send('I got a value of '+req.body.values);
+});
+
+app.get('/utc_date', function(req, responseHttp) {
+//	req.body.token
+	console.log("Asked for date...");
+	responseHttp.send(new Date().getTime());
 });
 
 
